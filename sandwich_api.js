5a89a793657e493772cf98a5290dc5a8a27561a9
pauldw <pwalker@fvml.ca>
2013-01-12 16:53:26 -0500

    Implemented original draft spec, with apologies for missing features.

--- sandwich_api.js
+++ sandwich_api.js
@@ -1,12 +1,76 @@
 var restify = require('restify');
 
-function respond(req, res, next) {
-  res.send('hello ' + req.params.name);
+// Simple administration authorization key
+var auth_key = "ABC123";
+
+// In-memory "database" for storing orders
+var order_id_counter = 0;
+var orders = {};
+
+function get_fillings(req, res, next) {
+  // Hard code the fillings into our code for now
+  var fillings = {"fillings": [
+    {'id': 1, 'name': 'Strawberry Jam'},
+    {'id': 2, 'name': 'Rasberry Jam'},
+    {'id': 3, 'name': 'Sliced Banana'},
+    {'id': 4, 'name': 'Peanut Butter'},
+    {'id': 5, 'name': 'Tahini'},
+    {'id': 6, 'name': 'Butter'},
+    {'id': 7, 'name': 'Salt'},
+    {'id': 8, 'name': 'Sugar'},
+    {'id': 9, 'name': 'Vegemite'}
+  ]};
+
+  res.send(fillings);
+}
+
+function post_new_order(req, res, next) {
+  
+  var fillings = req.params.fillings;
+  // TODO: check fillings are valid
+  
+  // Create a new order by first grabbing a unique ID
+  var id = order_id_counter;
+  order_id_counter += 1;
+  
+  orders[id] = {
+    "fillings": fillings,
+    "status": "queued"
+  };
+  
+  res.send({"id": id});
+}
+
+function get_order(req, res, next) {
+  var id = req.params.id;
+ 
+  res.send(orders[id]);
+}
+
+function update_order_status(req, res, next) {
+  var id = req.params.id;
+  var status = req.params.status;
+
+  // TODO: error on authorization fail
+  // TODO: check new status is a valid status
+  if(req.params.auth_key == auth_key)
+  {
+    orders[id]["status"] = status;
+  }
+  
+  res.send();
 }
 
 var server = restify.createServer();
-server.get('/hello/:name', respond);
-server.head('/hello/:name', respond);
+server.use(restify.bodyParser());
+
+// fillings
+server.get('/fillings', get_fillings);
+
+// orders
+server.post('/order', post_new_order);
+server.get('/order/:id', get_order);
+server.post('/order/:id/status', update_order_status);
 
 server.listen(8181, function() {
   console.log('%s listening at %s', server.name, server.url);
